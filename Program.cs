25a78d03151e5fa3b689e1b7a1945f266aa4c5f0
Cesar Carrasquel <cesar.carrasquel@gmail.com>
2022-10-05 16:21:16 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Linq;
+
+static IEnumerable<string> Personas(){
+    yield return "Francisco";
+    yield return "Misael";
+    yield return "Andres";
+    yield return "Pedro";
+    yield return "Pablo";
+    yield return "Javier";
+}
+
+/////////// DOS METODOS PARA CONSULTAS ////////
+// Query syntax (Se parece a SQL)
+//var personas = (from p in Personas()
+//            select p);
+
+// Method syntax (Se parece a expresiones lambda)
+var personas = Personas().Select(p => p);
+
+
+// ejecutar consulta
+// debemos recorrer en foreach, o utilizar ToList() o ToArray() 
+foreach(var p in personas)
+    Console.WriteLine(p);
